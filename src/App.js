1930fe700593953846f398569ce8a5a2762bfff3
Orivelton Cesar <orivelton10@gmail.com>
2019-11-26 22:13:04 +0000

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  increment = () => {
+
+  }
+
+  decrement = () => {
+
+  }
+
+  return (
+    <div className="App">
+      <h2>Counter</h2>
+    </div>
+  );
+}
+
+export default App;
